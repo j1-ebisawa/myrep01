bdc951af1962eec611784a8f843eee358994cd7c
Juichi Ebisawa <j1_ebisawa@amail.plala.or.jp>
2020-07-02 16:32:11 +0900

    初期登録

---
+++ ebi01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void){
+  float f = 3.14;
+  
+    int     nenrei=20;
+    printf("nenrei=%d\n",nenrei);
+
+    float   taijyu  = 65.5;
+    printf("taijyu~%f\n",taijyu);
+
+	nenrei=taijyu;
+    printf("nenrei=%d\n",nenrei);
+
+}
